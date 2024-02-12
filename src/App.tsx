4d3b41bf04767fff2b233a100f6c8b55ee273aa6
Yonathan Calderon <yonathanecalderon@gmail.com>
2024-02-11 22:50:09 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,77 @@
+import Home from "./pages/home/Home";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Users from "./pages/users/Users";
+import Products from "./pages/products/Products";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Menu from "./components/menu/Menu";
+import Login from "./pages/login/Login";
+import "./styles/global.scss";
+import User from "./pages/user/User";
+import Product from "./pages/product/Product";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+
+const queryClient = new QueryClient();
+
+function App() {
+  const Layout = () => {
+    return (
+      <div className="main">{/* Para agregarle archivos de estilos de manera general */}
+        <Navbar />{/* Componente compun a todas las páginas de la navegación */}
+        {/*Una observación general en base al ToDoList que ya hice, sería tener una secció SectionMain.jsx y ahi meter los componentes que vienen abajo */}
+        <div className="container">{/* Container para las diferentes páginas de la navegación */}
+          <div className="menuContainer">{/* Container para el menú de navegación */}
+            <Menu />{/* El menú se carga a traves del archivoi data.ts con map */}
+          </div>
+          <div className="contentContainer"> {/* Las diferentes páginas de la navegación */}
+            <QueryClientProvider client={queryClient}>
+              <Outlet /> {/* Con este elemento la navegación se hace de manera dinamica */}
+            </QueryClientProvider>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/users",
+          element: <Users />,
+        },
+        {
+          path: "/products",
+          element: <Products />,
+        },
+        {
+          path: "/users/:id",
+          element: <User />,
+        },
+        {
+          path: "/products/:id",
+          element: <Product />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
+
+export default App;
